dee749bf98d9c110a19c4c95b274f7a0f5ac350d
just2102 <thesixteensaltines@gmail.com>
2023-09-17 10:51:09 +0700

    get all posts (+ pagination) and get posts by id

--- livej-server/src/post/post.controller.ts
+++ livej-server/src/post/post.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { Prisma, Post as JPost } from '@prisma/client';
 import { PostService } from './post.service';
 
-@Controller('post')
+@Controller('posts')
 export class PostController {
   constructor(private postService: PostService) {}
+
   @Post('/createPost')
   async createPost(@Body() body: Prisma.PostCreateInput): Promise<JPost> {
     return await this.postService.createPost(body);
+  }
+
+  @Get('/getAllPosts')
+  async getAllPosts(@Query('pages') pages: number): Promise<JPost[]> {
+    return await this.postService.getAllPosts(pages);
+  }
+
+  @Get('/getPostById')
+  async getPostById(@Query('id') id: string): Promise<JPost | null> {
+    return await this.postService.getPostById(id);
   }
 }
--- livej-server/src/post/post.service.ts
+++ livej-server/src/post/post.service.ts
@@ -5,9 +5,35 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class PostService {
   constructor(private prisma: PrismaService) {}
+
   async createPost(createPostDto: Prisma.PostCreateInput): Promise<Post> {
     return await this.prisma.post.create({
       data: createPostDto,
+    });
+  }
+
+  async getAllPosts(pages = 1): Promise<Post[]> {
+    const numOfPostsOnPage = 10;
+    const maxPagesLimit = 100;
+    if (pages < 1 || pages > maxPagesLimit) {
+      throw new Error(
+        `Invalid page number. Must be between 1 and ${maxPagesLimit}`,
+      );
+    }
+
+    const skip = (pages - 1) * numOfPostsOnPage;
+
+    return await this.prisma.post.findMany({
+      skip: skip,
+      take: numOfPostsOnPage,
+    });
+  }
+
+  async getPostById(id: string): Promise<Post | null> {
+    return await this.prisma.post.findFirst({
+      where: {
+        id: id,
+      },
     });
   }
 }
